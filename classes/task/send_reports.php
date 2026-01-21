b2f81f93d0edf7e7da135e6a476253498c85acdc
simphiwe-nkabin <simphiwe@shaper.co.za>
2026-01-21 16:45:43 +0200

    improve sql query to return accurate data

--- classes/task/send_reports.php
+++ classes/task/send_reports.php
@@ -27,92 +27,6 @@ class send_reports extends \core\task\scheduled_task {
 
         global $DB, $CFG;
 
-        // $tagname = get_config('local_gradereports', 'course_tag');
-        // // Make sure tag API is available
-        // require_once($CFG->dirroot . '/tag/lib.php');
-
-        // //  Get tags attached to this assignment activity.
-        // $tag = \core_tag_tag::get_by_name(0, $tagname);
-
-        // if (!$tag) {
-        //     return;
-        // }
-        // $tagid = $tag->id;
-
-        // // get tagged courses
-        // $sql = "
-        //     SELECT c.id, c.fullname, c.shortname
-        //     FROM {course} c
-        //     JOIN {tag_instance} ti ON ti.itemid = c.id
-        //     WHERE ti.itemtype = 'course'
-        //     AND ti.tagid = :tagid
-        //     AND c.id <> :siteid
-        // ";
-        // $params = [
-        //     'tagid'  => $tagid,
-        //     'siteid' => SITEID,
-        // ];
-
-        // $courses_data = $DB->get_records_sql($sql, $params);
-        // $indexed_courses = array_values($courses_data);
-        
-        // $groupstr = get_config('local_gradereports', 'groups');
-        // $groups = !empty($groupstr) ? explode(',', $groupstr) : [];
-        
-        // $quizzes_data = $DB->get_records('quiz', array('course' => $indexed_courses[0]->id), '', 'id, course, name');
-        // $indexed_quizzes = array_values($quizzes_data);
-
-        // $assignments_data = $DB->get_records('assign', array('course' => $indexed_courses[0]->id), '', 'id, course, name');
-        // $indexed_assignments = array_values($assignments_data);
-
-
-        // echo "\ncourses: " . json_encode($indexed_courses);
-        // echo "\ngroups: " . json_encode($groups);
-        // echo "\nquizzes: " . json_encode($indexed_quizzes);
-        // echo "\nassignments: " . json_encode($indexed_assignments);
-
-        // $tagged_activities_sql = "
-        //     SELECT
-        //         cm.id AS cmid,
-        //         cm.module,
-        //         m.name AS modulename,
-        //         cm.instance,
-        //         q.id AS quizid,
-        //         q.name AS quizname,
-        //         a.id AS assignid,
-        //         a.name AS assignname,
-        //         c.fullname AS coursename
-        //     FROM {course_modules} cm
-        //     JOIN {modules} m ON m.id = cm.module
-        //     JOIN {course} c ON c.id = cm.course
-        //     JOIN {tag_instance} ti ON ti.itemid = cm.id
-        //     LEFT JOIN {quiz} q ON q.id = cm.instance AND m.name = 'quiz'
-        //     LEFT JOIN {assign} a ON a.id = cm.instance AND m.name = 'assign'
-        //     WHERE cm.course = :courseid
-        //     AND ti.itemtype = 'course_modules'
-        //     AND ti.tagid = :tagid
-        //     AND m.name IN ('quiz', 'assign')
-        //     AND cm.deletioninprogress = 0
-        // ";
-        // $tagged_activities_params = [
-        //     'courseid' => $indexed_courses[0]->id,
-        //     'tagid'    => 7,
-        // ];
-
-        // $activities = $DB->get_records_sql($tagged_activities_sql, $tagged_activities_params);
-        // $indexed_activities = array_values($activities);
-
-
-
-        // // get group members
-        // $group_members = $DB->get_records('group_members', array('groupid' => 1));
-        // $quiz_grades = $DB->get_records('quiz_grades', array('userid' => array_values($group_members)[1]->userid));
-
-        // api_helper::send_report(array('courses' => $indexed_courses, 'groups' => $groups, 'quizzes' => $indexed_quizzes, 'assignments'=> $indexed_assignments, 'activities' => $indexed_activities));
-
-
-        // -------------------------------------------------------------
-
         // Make sure tag API is available
         require_once($CFG->dirroot . '/tag/lib.php');
 
@@ -150,105 +64,89 @@ class send_reports extends \core\task\scheduled_task {
         $groupstr = get_config('local_gradereports', 'groups');
         $groupids = !empty($groupstr) ? explode(',', $groupstr) : [];
 
-        echo "\n courseids:" . implode(",", $courseids);
-        echo "\n groupids:" . implode(",", $groupids);
-        echo "\n activity_tagid:" . $activity_tagid;
-        
-
         list($courseinsql, $courseparams) = $DB->get_in_or_equal($courseids, SQL_PARAMS_NAMED, 'courseid');
         list($groupinsql,  $groupparams)  = $DB->get_in_or_equal($groupids,  SQL_PARAMS_NAMED, 'groupid');
-        list($taginsql,    $tagparams)    = $DB->get_in_or_equal([$activity_tagid],    SQL_PARAMS_NAMED, 'tagid');
+        list($taginsql,    $tagparams)    = $DB->get_in_or_equal($activity_tagid,    SQL_PARAMS_NAMED, 'tagid');
 
         $params = array_merge($courseparams, $groupparams, $tagparams);
 
         $sql = "
-            SELECT
-                c.id               AS courseid,
-                c.fullname         AS coursename,
-
-                g.id               AS groupid,
-                g.name             AS groupname,
-
-                u.id               AS userid,
+            SELECT 
+                gg.id AS gradeid,
+                c.fullname AS coursename,
+                g.name AS groupname,
+                u.id AS userid,
                 u.firstname,
                 u.lastname,
-                u.email,
-
-                m.name             AS moduletype,
-                cm.id              AS cmid,
-                gi.iteminstance    AS activityid,
-
-                gi.itemname        AS activityname,
+                gi.itemmodule AS activitytype,
+                gi.itemname AS activityname,
                 gg.finalgrade,
                 gi.grademax,
-
-                -- Due date
-                CASE 
-                    WHEN m.name = 'assign'  THEN a.duedate
-                    WHEN m.name ='quiz'     THEN q.timeclose
+                
+                CASE
+                    WHEN gi.itemmodule = 'assign' THEN a.duedate
+                    WHEN gi.itemmodule = 'quiz' THEN q.timeclose
                 END AS duedate,
-
-                -- Submission date
+                
                 CASE 
-                    WHEN m.name = 'assign'  THEN s.timemodified
-                    WHEN m.name = 'quiz'    THEN qa.timemodified
+                    WHEN gi.itemmodule = 'assign' THEN a_s.timemodified
+                    WHEN gi.itemmodule = 'quiz' THEN qa.timemodified
                 END AS submissiondate
 
-            FROM {course} c
+            FROM mdl_grade_grades gg
 
-            JOIN {groups} g
-                ON g.courseid = c.id
+            JOIN mdl_user u
+                ON u.id = gg.userid
+            
+            JOIN mdl_groups_members gm
+                ON gm.userid = u.id
 
-            JOIN {groups_members} gm
-                ON gm.groupid = g.id
+            JOIN mdl_groups g
+                ON g.id = gm.groupid
 
-            JOIN {user} u
-                ON u.id = gm.userid
+            JOIN mdl_grade_items gi
+                ON gi.id = gg.itemid
+                
+            JOIN mdl_course c
+                ON c.id = gi.courseid
+                
+            LEFT JOIN mdl_assign a
+                ON a.id = gi.iteminstance
+                AND gi.itemmodule = 'assign'
+            
+            LEFT JOIN mdl_quiz q
+                ON q.id = gi.iteminstance
+                AND gi.itemmodule = 'quiz'
+                
+            JOIN mdl_course_modules cm
+                ON cm.instance = gi.iteminstance
 
-            JOIN {course_modules} cm
-                ON cm.course = c.id
-            AND cm.deletioninprogress = 0
-
-            JOIN {modules} m
+            JOIN mdl_modules m
                 ON m.id = cm.module
-            AND m.name IN ('quiz', 'assign')
+                AND m.name IN ('quiz', 'assign')
 
-            JOIN {tag_instance} ti
-                ON ti.itemtype = 'course_modules'
-            AND ti.itemid = cm.id
-            AND ti.tagid $taginsql
-
-            JOIN {grade_items} gi
-                ON gi.courseid = c.id
-            AND gi.itemmodule = m.name
-            AND gi.iteminstance = cm.instance
-
-            LEFT JOIN {grade_grades} gg
-                ON gg.itemid = gi.id
-            AND gg.userid = u.id
-
-            -- Assignment dates
-            LEFT JOIN {assign} a
-                ON a.id = cm.instance
-                AND m.name = 'assign'
+            JOIN mdl_tag_instance ti
+                ON ti.itemid = cm.id
+                AND ti.itemtype = 'course_modules'
+                
+            JOIN mdl_tag t
+                ON t.id = ti.tagid
             
-            LEFT JOIN {assign_submission} s
-                ON s.assignment = a.id
-                AND s.userid = u.id
-                AND s.latest = 1
-            
-            -- Quiz dates
-            LEFT JOIN {quiz} q
-                ON q.id = cm.instance
-                AND m.name = 'quiz'
-
-            LEFT JOIN {quiz_attempts} qa
+            LEFT JOIN mdl_quiz_attempts qa
                 ON qa.quiz = q.id
                 AND qa.userid = u.id
-                AND qa.state = 'finished'
+                AND qa.attempt = 1
+                
+            LEFT JOIN mdl_assign_submission a_s
+                ON a_s.assignment = a.id
+                AND a_s.userid = u.id
+                AND a_s.status = 'submitted'
 
-            WHERE c.id $courseinsql
+            WHERE gg.finalgrade IS NOT NULL
             AND g.id $groupinsql
+            AND gi.itemmodule IN ('quiz', 'assign')
+            AND gi.courseid $courseinsql
+            AND ti.tagid $taginsql
         ";
 
         $records = $DB->get_records_sql($sql, $params);
